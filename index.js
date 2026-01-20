2c37d99ef6747affeb0cb8cabc907e804202c3b1
Aditya Mishra <3567adi@users.noreply.github.com>
2026-01-20 12:06:42 +0530

    Fix root route and update backend config

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 import express from 'express';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
 import chatbotRoutes from './routes/chatbot.route.js';
 
+// dotenv.config();
+const app = express();
 
-const app = express()
-dotenv.config()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 10000;
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+
+console.log("MONGO_URI:", process.env.MONGO_URI)
 
 
 //Database Connection code
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-
-  }).catch((error) => {
-    console.log("Error connecting to MongoDB:", error);
-
-  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.log("Error connecting to MongoDB:", error));
 
 // Defining Routes
 
-app.use("/bot/v1/", chatbotRoutes)
+app.use("/bot/v1/", chatbotRoutes);
 
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+  console.log(`Server is running on port ${port}`);
 })
